06fc06cc0fa937c1603b5922dfaa00b952f38afb
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-09-05 17:29:32 +0530

    Creating indian census analyzer program

---
+++ IndianCensusAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianCensusAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian Census Analyzer");
+        }
+    }
+}
